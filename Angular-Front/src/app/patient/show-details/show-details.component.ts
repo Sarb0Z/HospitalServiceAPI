c83cb209632f7e60ddfa7fd195d249e262b2359b
Sarb0z <abdulrafay972@gmail.com>
2022-07-28 21:27:13 +0500

    Merged with master

---
+++ Angular-Front/src/app/patient/show-details/show-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-show-details',
+  templateUrl: './show-details.component.html',
+  styleUrls: ['./show-details.component.css']
+})
+export class ShowDetailsComponent implements OnInit {
+  @Input() patient:any;
+  @Output('closeClick') closeClick: EventEmitter<any> = new EventEmitter();
+
+  id:number=0;
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.patient.id);
+  }
+
+}
