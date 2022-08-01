3455a35e74ee9c97c9517927915b85e84a833fe4
Sarb0z <abdulrafay972@gmail.com>
2022-08-01 17:50:20 +0500

    Added double click and edit freeze bug in Patient Details

--- Angular-Front/src/app/Services/DoctorApi/doctor-api.service.ts
+++ Angular-Front/src/app/Services/DoctorApi/doctor-api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Doctor } from '../Interfaces/doctor';
+import { Doctor } from 'src/app/Interfaces/doctor';
 
 @Injectable({
   providedIn: 'root'
